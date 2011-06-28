2aeae40cc56ab342eda0b3ad94365de00f4dfac5
Mark Daggett <mark@humansized.com>
2011-06-28 15:45:01 -0500

    updates to the calculations so that we can correctly draw the funnel based on the median and max values.

--- javascripts/funnelplot.highcharts.js
+++ javascripts/funnelplot.highcharts.js
@@ -7,9 +7,46 @@ $.fn.funnelPlot = function(options) {
         max: function(arr) {
             return Math.max.apply(Math, arr);
         },
+        median: function(arr) {
+          var len = arr.length;
+            var i = Math.floor(len / 2);
+            if ((len % 2) == 1) {
+                return (arr[i]);
+            } else {
+                return (arr[i - 1] + arr[i]) / 2;
+            }
+        },
+        stats: function(arr) {
+            arr.sort(function(l, r) {
+                return l - r;
+            });
+            var hsh = {
+                max: this.max(arr),
+                min: this.min(arr),
+                sum: 0,
+                sumSq: 0,
+                count: arr.length,
+                range: Number.NaN,
+                arithMean: Number.NaN,
+                standardDeviation: Number.NaN,
+                median: arr[0]
+            };
+            hsh.range = hsh.max - hsh.min;
+            for (var x = 0; x < hsh.count; x++) {
+                hsh.sum += x;
+                hsh.sumSq += x * x;
+            }
+            hsh.arithMean = hsh.sum / hsh.count;
+            if (hsh.count > 0) {
+                hsh.standardDeviation = Math.sqrt(((hsh.count * hsh.sumSq) - (hsh.sum * hsh.sum)) / (hsh.count * (hsh.count - 1)));
+                hsh.median = this.median(arr);
+            }
+            return hsh;
+        },
         funnelCurve: function(opts) {
             var options = {
-                type: 'line',
+                type: 'spline',
+                showInLegend: false,
                 name: ' ',
                 data: [],
                 marker: {
@@ -30,23 +67,26 @@ $.fn.funnelPlot = function(options) {
             var xData = [];
             var yData = [];
             var parts = [];
-            for (var x = 0; x < len; x++) {
-                xData.push(data[x][0]);
-                yData.push(data[x][1]);
+            for (var n = 0; n < len; n++) {
+                xData.push(data[n][0]);
+                yData.push(data[n][1]);
             }
-            var xMin = this.min(xData);
-            var xMax = this.max(xData);
-            var yMin = this.min(yData);
-            var yMax = this.max(yData);
+            var x = this.stats(xData);
+            var y = this.stats(yData);
             parts.push(this.funnelCurve({
-                data: [[xMin, (yMax / 2)], [xMax, yMax]]
+                color: "#0000aa",
+                dashStyle: "ShortDash",
+                data: [[x.min, y.median], [x.max, y.max]]
             }));
             parts.push(this.funnelCurve({
-                name : 'Average',
-                data: [[xMin, (yMax / 2)], [xMax, (yMax / 2)]]
+                name: 'Median',
+                showInLegend: true,
+                data: [[x.min, y.median], [x.max, y.median]]
             }));
             parts.push(this.funnelCurve({
-                data: [[xMin, (yMax / 2)], [xMax, yMin]]
+                color: "#0000aa",
+                dashStyle: "ShortDash",
+                data: [[x.min, y.median], [x.max, y.min]]
             }));
             return (parts);
         }
